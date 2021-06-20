375ef1ac06c28a8c5406966e6ed72d7e9c4511c4
midigo0411 <midigo041@gmail.com>
2021-06-20 11:52:48 +0300

    add a comment page

--- app/main/views.py
+++ app/main/views.py
@@ -54,3 +54,20 @@ def categories(id):
 
     pitches = Pitch.get_pitches_by_category(id)
     return render_template('home.html', title = title, form=form, pitches = pitches)
+
+@main.route('/<pitch_id>/comments', methods = ['GET', 'POST'])
+def comment(pitch_id):
+
+    comment_form = CommentForm()
+    title = 'Comments | Pitcher'
+    pitch = Pitch.query.filter_by(id=pitch_id).first()
+    # Get comments for pitch.
+    comments = Comment.get_comments(pitch_id)
+    if comment_form.validate_on_submit():
+        comment = comment_form.comment.data
+
+        new_comment = Comment(comment=comment, user=current_user, pitch=pitch)
+        new_comment.save_comment()
+        return redirect(request.referrer)
+
+    return render_template('comments.html', pitch=pitch, form=comment_form, comments=comments, title=title)
